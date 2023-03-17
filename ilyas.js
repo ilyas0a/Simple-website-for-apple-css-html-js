d13469af31aac2b645a1ef1d5dce28744d840edc
ilyas0a <115528846+ilyas0a@users.noreply.github.com>
2023-03-17 15:18:20 +0300

    Add files via upload

---
+++ ilyas.js
@@ -0,0 +1,9 @@
+let javas=document.querySelector('.javas');
+let continer = document.querySelector('.continer')
+function phones(phone){
+    javas.src=phone;
+
+}
+function colors(color){
+continer.style.background= color;
+}
